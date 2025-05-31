a26c8232b796fc925cd6f9244faa163899421ddb
Ricardo Barrios <rjbm29@gmail.com>
2025-05-31 18:23:29 -0500

    update pages module

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,7 @@
 export default function dashboard() {
   return (
     <>
-      <div className="bg-red-600">olamundo</div>
+      <div className="bg-(--background-color) h-screen w-screen">olamundo</div>
     </>
   );
 }
