caf08071d182a4613c3f8988a0c8f616635263ea
Ricardo Barrios <rjbm29@gmail.com>
2025-06-07 12:32:52 -0500

    add section title component

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,13 @@
 // import HistoricList from "../../components/historic-list.component";
+import { ReactNode } from "react";
 import IndicatorBar from "../../components/Indicator-card.component";
 import { indicatorsArray } from "../../types/dashboard.types";
 
 export default function dashboard() {
+  const sectionTitle = (title: string): ReactNode => {
+    return <h3 className="row-span-1 flex items-center font-[600]">{title}</h3>;
+  };
+
   return (
     <>
       <div className="col-span-20 h-full w-full grid grid-flow-col grid-cols-20 gap-4">
@@ -12,18 +17,20 @@ export default function dashboard() {
               <IndicatorBar {...item} key={index} />
             ))}
           </section>
-          <h3 className="row-span-1 flex items-center font-[500]">
-            Flujo de Caja
-          </h3>
+
+          {sectionTitle("Flujo de Caja")}
+
           <section className="bg-gray-400 row-span-6">olamundo</section>
-          <h3 className="row-span-1 flex items-center font-[500]">
-            Movimientos Recientes
-          </h3>
+
+          {sectionTitle("Movimiemtos Recientes")}
+
           <section className="bg-gray-400 row-span-6"></section>
         </div>
         <div className="col-span-5 grid gap-4 grid-flow-row grid-rows-16 ">
-          <section className="bg-blue-400 row-span-8">olamundo</section>
-          <section className="bg-blue-400 row-span-8">olamundo</section>
+          {sectionTitle("Variable")}
+          <section className="bg-blue-400 row-span-7"></section>
+          {sectionTitle("Tareas pendientes")}
+          <section className="bg-blue-400 row-span-7">olamundo</section>
         </div>
       </div>
     </>
